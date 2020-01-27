dfe692c7cca0bc178b31a3499a65501418040218
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-01-26 22:13:00 -0300

    Style for app with bootstrap

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const sizeOf = require('image-size');
+const ExifImage = require('exif').ExifImage;
 
 const {app, BrowserWindow, ipcMain} = electron;
 let mainWindow;
@@ -8,14 +9,22 @@ app.on('ready', () => {
    mainWindow = new BrowserWindow({
        webPreferences: {
            nodeIntegration: true
-       }
+       },
+       width: 400,
+       resizable: false
    });
-   
+   mainWindow.setMenu(null);
    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('obterDimensoesDaImagem', (event, path)=> {
     sizeOf(path, function(err, dimensions) {
         mainWindow.webContents.send('dimensoesDaImagem', dimensions);
+    });
+});
+
+ipcMain.on('obterMetaDadosImage', (event, path) => {
+    new ExifImage({ image: path}, (error, exifData) => {
+        mainWindow.webContents.send('metaDadosImage', exifData);
     });
 });
